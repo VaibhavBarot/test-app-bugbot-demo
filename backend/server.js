2cfcc20171a1e8d39a3be2087592a57f53c44091
vbarot12 <vbarot@edgeho.me>
2025-11-23 08:26:19 -0500

    product 4

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ let cart = {
 const products = [
   { id: 1, name: 'Product 1', price: 10.99 },
   { id: 2, name: 'Product 2', price: 19.99 },
-  { id: 3, name: 'Product 3', price: 29.99 }
+  { id: 3, name: 'Product 3', price: 29.99 },
+  { id: 4, name: 'Product 4', price: 39.99 }
 ];
 
 // Get products
